533f7aa072e97602e3d990a7df2ca9d844bfabc4
toir02 <wend8765@gmail.com>
2024-04-12 14:38:31 +0300

    created post method for create object of record

--- records/views.py
+++ records/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import (
+    generics,
+    status
+)
+from rest_framework.response import Response
 
-# Create your views here.
+from records.models import Record
+from records.serializers import RecordSerializer
+
+
+class RecordCreateAPIView(generics.CreateAPIView):
+    serializer_class = RecordSerializer
+
+    def post(self, request, *args, **kwargs):
+        data = self.request.data.get("data")
+        if not data:
+            return Response(
+                data={
+                    "error": "data field is required"
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        instance = Record.objects.create(
+            data=data
+        )
+
+        serializer = self.serializer_class(instance)
+        return Response(serializer.data)

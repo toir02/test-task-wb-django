d0af165ffeed26edc2ba3cf85b9abac2147edd59
toir02 <wend8765@gmail.com>
2024-04-12 14:34:34 +0300

    created model Record with initial migration

--- records/models.py
+++ records/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Record(models.Model):
     data = models.JSONField(verbose_name="Данные для записи")
+
+    class Meta:
+        verbose_name = "запись"
+        verbose_name_plural = "записи"
